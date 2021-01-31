0a0cca00f17a6ccf4d1ae2197d652dbf7662fe80
tanya <9328076858a@gmail.com>
2021-01-31 17:29:01 +0530

    mango

---
+++ js/Stone.js
@@ -0,0 +1,31 @@
+class Stone {
+    constructor(x,y,r)
+    {
+        var options={
+            isStatic:false,
+            restitution:0,
+            friction:1,
+            density:1.2
+
+        }
+        this.x=x;
+        this,y=y;
+        this.r=r
+        this.image=loadImage("images/stone.png")
+        this.body=Bodies.circle(this.x,this.y,this.r,options)
+        World.add(world,this.body);
+    }
+    display(){
+        
+        var stonePosition=this.body.position;	
+		push()
+		//translate(stonePosition.x, stonePosition.y);
+		// rectMode(CENTER);
+		//rotate(this.body.angle)
+		fill(255,0,255)
+		imageMode(CENTER);
+		
+		image(this.image, this.body.position.x, this.body.position.y,this.r*2, this.r*2)
+		pop()  
+    } 
+}
